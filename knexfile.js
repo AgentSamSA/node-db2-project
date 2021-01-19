7d011210787e427d12079ce7c47e542ca3d04cba
Samuel Lee <samuelxlee@gmail.com>
2021-01-19 12:41:16 -0800

    added modules and knex and sqlite3 libraries, created knex file

---
+++ knexfile.js
@@ -0,0 +1,25 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    },
+    useNullasDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  staging: {
+  },
+
+  production: {
+  }
+
+};
