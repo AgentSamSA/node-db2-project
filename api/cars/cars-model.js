24419ba6ea0bbce47318e2d3585d4dc160bfde40
Samuel Lee <samuelxlee@gmail.com>
2021-01-19 13:24:17 -0800

    wrote the model for different endpoint operations, wrote the endpoints for the cars

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -5,5 +5,31 @@ module.exports = {
     getById,
     insert,
     update,
-    delete
+    remove
+};
+
+function get() {
+    return db('cars');
+}
+
+function getById(id) {
+    return db('cars').where('car_id', id).first();
+}
+
+function insert(car) {
+    return db('cars').insert(car)
+        .then(([id]) => {
+            return db('cars').where('car_id', id).first();
+        });
+}
+
+function update(id, car) {
+    return db('cars').where('car_id', id).update(car)
+        .then(([id]) => {
+            return db('cars').where('car_id', id).first();
+        });
+}
+
+function remove(id) {
+    return db('cars').where('car_id', id).del();
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -2,3 +2,62 @@ const express = require('express');
 const Car = require("./cars-model");
 
 const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    Car.get()
+        .then(cars => {
+            res.status(200).json(cars);
+        })
+        .catch(next);
+});
+
+router.get("/:id", (req, res, next) => {
+    const { id } = req.params;
+
+    Car.getById(id)
+        .then(car => {
+            res.status(200).json(car);
+        })
+        .catch(next);
+});
+
+router.post("/", (req, res, next) => {
+    Car.insert(req.body)
+        .then(car => {
+            res.status(201).json(car);
+        })
+        .catch(next);
+});
+
+router.put("/:id", (req, res, next) => {
+    const { id } = req.params;
+
+    Car.update(id, req.body)
+        .then(car => {
+            res.status(200).json(car);
+        })
+        .catch(next);
+});
+
+router.delete("/:id", (req, res, next) => {
+    const { id } = req.params;
+
+    Car.delete(id)
+        .then(count => {
+            res.status(200).json({ message: "the car data has been deleted" });
+        })
+        .catch(next);
+});
+
+router.use((err, req, res, next) => {
+    const env = process.env.NODE_ENV || "development";
+    const message = env === "development"
+        ? err.message
+        : "something went wrong!"
+    res.status(500).json({
+        message: message,
+        stack: err.stack
+    });
+});
+
+module.exports = router;
